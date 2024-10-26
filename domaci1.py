7784d9e89113aa3747a85b790c91185f0cc757d1
ivanajoo <151941626+ivanajoo@users.noreply.github.com>
2024-10-26 15:22:16 +0200

    Add files via upload

---
+++ domaci1.py
@@ -0,0 +1,10 @@
+#kako da promjenimo prvu stavku iz liste
+#kako da obrisem posljednju stavku iz liste
+
+games = ["Call of Duty" , "League of legends" , "Fortnite"]
+games[0]="Black Ops"
+print(games)
+
+games.remove(games[0])
+
+print(games)
